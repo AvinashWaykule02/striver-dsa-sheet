a056bb0a90fe67bdfedede5c55b27c528de3bffc
AvinashWaykule02 <arunwaykule42@gmail.com>
2025-07-29 18:52:41 +0530

    largestElement program

--- Arrays/Easy/largestElement.cpp
+++ Arrays/Easy/largestElement.cpp
@@ -1,3 +1,16 @@
-//Given an array of integers nums, return the value of the largest element in the array
+//https://www.geeksforgeeks.org/problems/largest-element-in-array4009/1
 
+class Solution {
+  public:
+    int largest(vector<int> &nums) {
+        
+        int n = nums.size();
+        int maxEle = INT_MIN;
+        for(int i =0;i<n;i++) {
+            int ele = nums[i];
+            maxEle = max(maxEle, ele);
+        }
+        return maxEle;
+    }
+};
 
