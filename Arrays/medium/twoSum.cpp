3626df722bb109c91e1236b3e8fa8679008bc4da
AvinashWaykule02 <arunwaykule42@gmail.com>
2025-07-29 20:46:48 +0530

    updated two sum

--- Arrays/medium/twoSum.cpp
+++ Arrays/medium/twoSum.cpp
@@ -3,35 +3,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        int first = 0, second = 0;
-        map<int, vector<int>> m;
-
-        // Build the map with indices
-        for(int i = 0; i < n; i++) {
-            m[nums[i]].push_back(i);
-        }
-
-        for(int i = 0; i < n; i++) {
-            int ele = nums[i];
-            int find = target - ele;
-
-            if (m.find(find) != m.end()) {
-                if (find == ele) {
-                    if (m[ele].size() > 1) {
-                        first = m[ele][0];
-                        second = m[ele][1];
-                        break;
-                    }
-                } 
-                else {
-                    first = i;
-                    second = m[find][0]; 
-                    break;
-                }
+        int n =nums.size();
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++){
+            int complement = target - nums[i];
+            if(m.count(complement)){
+                return {i,m[complement]};
             }
+           m[nums[i]]=i;
         }
-
-        return {first, second};
+        return {};
     }
 };
